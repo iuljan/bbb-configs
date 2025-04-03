7112842a562378ff3b6b0ad4759a1f8ec57e1787
nailend <nailend@posteo.de>
2025-04-03 13:44:04 +0200

    Revert "Add sleep to massupgrade"
    This reverts commit 83a0be43f6f4e0eaa37f9855c4ef5d4f3c0d4321.

--- mass-update.sh
+++ mass-update.sh
@@ -57,24 +57,12 @@ for FILE_PATH in "${SORTED_FILES[@]}"; do
     echo "- $(basename "$FILE_PATH")"
 done
 echo ""
-read -rp "Do you want to proceed now or later [y/N/l]? " choice
+read -rp "Do you want to proceed [y/N]? " choice
 echo ""
-if [[ "$choice" =~ ^[Ll]$ ]]; then
-    read -rp "Enter the number of seconds to sleep: " seconds
-    if [[ "$seconds" =~ ^[0-9]+$ ]]; then
-        echo "Sleeping for $seconds seconds..."
-        sleep "$seconds"
-    else
-        echo "Invalid input. Exiting..."
-        exit 1
-    fi
-elif [[ ! "$choice" =~ ^[Yy]$ ]]; then
+if [[ ! "$choice" =~ ^[Yy]$ ]]; then
     echo "Exiting..."
     exit 0
 fi
-
-echo "Proceeding..."
-
 
 # Function to check reachability
 check_reachability() {
